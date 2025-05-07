68c20f5b3b3e814655346b70b500fc77fc14148d
Victhor CM <vcabralmagalhaes@gmail.com>
2025-05-07 15:12:53 -0300

    edit,create e delete views

--- sistema/veiculos/urls.py
+++ sistema/veiculos/urls.py
@@ -3,5 +3,8 @@ from django.contrib.auth.decorators import login_required, permission_required
 from .views import *
 
 urlpatterns= [
-    path('', login_required(ListarVeiculos.as_view(),login_url='login'), name='listar-veiculos')
+    path('', login_required(ListarVeiculos.as_view(),login_url='login'), name='listar-veiculos'),
+    path('cadastrar',login_required(CadastrarVeiculos.as_view(),login_url='login'), name='cadastrar-veiculos'),
+    path('editar/<str:pk>',login_required(EditarVeiculos.as_view(),login_url='login'), name='editar-veiculos'),
+    path('excluir/<str:pk>',login_required(ExcluirVeiculos.as_view(),login_url='login'), name='excluir-veiculos'),
 ]
--- sistema/veiculos/views.py
+++ sistema/veiculos/views.py
@@ -1,7 +1,29 @@
 from .models import Veiculo
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import VeiculoForm
+from django.urls import reverse_lazy
 
 class ListarVeiculos(ListView):
     model = Veiculo
     context_object_name = 'veiculos'
     template_name = 'veiculos/listar.html'
+
+class CadastrarVeiculos(CreateView):
+    model = Veiculo
+    form_class = VeiculoForm
+    context_object_name = 'veiculos'
+    template_name = 'veiculos/cadastrar.html'
+    success_url = reverse_lazy('listar-veiculos') 
+
+class EditarVeiculos(UpdateView):
+    model = Veiculo
+    form_class = VeiculoForm
+    context_object_name = 'veiculos'
+    template_name = 'veiculos/cadastrar.html'
+    success_url = reverse_lazy('listar-veiculos') 
+
+class ExcluirVeiculos (DeleteView):
+    model = Veiculo
+    context_object_name = 'veiculos'
+    template_name = 'alert.html'
+    success_url = reverse_lazy('listar-veiculos') 
